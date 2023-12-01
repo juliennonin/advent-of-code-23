07c117cd194175d4f2c0fe81ab53a9f7bcac9ead
JulienNonin <julien.nonin@sfr.fr>
2023-12-01 09:04:16 +0100

    (helper) Add puzzle helper

---
+++ py/helpers/load_puzzle.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATA_FOLDER = os.path.join(os.path.dirname(__file__), "../../data")
+AOC_SESSION_COOKIE = os.environ["AOC_SESSION_COOKIE"]
+
+
+def download_puzzle(day_num):
+    file_name = f"day{day_num}.txt"
+
+    url = f"https://adventofcode.com/2023/day/{day_num}/input"
+    cookies = {"session": AOC_SESSION_COOKIE}
+
+    r = requests.get(url, cookies=cookies)
+    if r.status_code == 200:
+        with open(os.path.join(DATA_FOLDER, file_name), "w") as f:
+            f.write(r.text)
+    else:
+        raise Exception(f"Error downloading puzzle: {r.status_code}")
+
+
+def puzzle(day_num):
+    file_name = f"day{day_num}.txt"
+
+    if file_name not in os.listdir(DATA_FOLDER):
+        download_puzzle(day_num)
+
+    assert file_name in os.listdir(DATA_FOLDER)
+    return os.path.join(DATA_FOLDER, file_name)
