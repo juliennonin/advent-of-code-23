e4db1a4783ceead9e6a53efacfa9bbfbde05f22b
JulienNonin <julien.nonin@sfr.fr>
2023-12-06 13:51:19 +0100

    (day06) part2

--- py/day06.py
+++ py/day06.py
@@ -19,9 +19,13 @@ if __name__ == "__main__":
     with open(puzzle(6), "r") as f:
         time_line, record_line = f.read().splitlines()
 
-    times = list(map(int, re.findall(r"\d+", time_line)))
-    records = list(map(int, re.findall(r"\d+", record_line)))
+    times_str = re.findall(r"\d+", time_line)
+    records_str = re.findall(r"\d+", record_line)
+
+    times, records = list(map(int, times_str)), list(map(int, records_str))
+    one_time, one_record = int("".join(times_str)), int("".join(records_str))
 
     print("Part 1 —", math.prod(map(number_of_new_records, times, records)))
+    print("Part 2 —", number_of_new_records(one_time, one_record))
 
 # %%
