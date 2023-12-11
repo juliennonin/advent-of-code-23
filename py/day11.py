fe836b0cf60cf641ab4c8b25cd68f5a424b93894
JulienNonin <julien.nonin@sfr.fr>
2023-12-11 22:13:20 +0100

    (day11) part2

--- py/day11.py
+++ py/day11.py
@@ -3,20 +3,37 @@ import numpy as np
 
 from helpers.load_puzzle import puzzle
 
+
 # %%
-with open(puzzle(11), 'r') as f:
+def get_galaxies_coords(universe, expansion_factor):
+    coords = np.argwhere(universe)
+    new_coords = np.copy(coords)
+
+    offset_x = np.where(np.sum(universe, axis=1) == 0)[0]
+    offset_y = np.where(np.sum(universe, axis=0) == 0)[0]
+    for x in offset_x:
+        new_coords[coords[:, 0] >= x, 0] += expansion_factor - 1
+    for y in offset_y:
+        new_coords[coords[:, 1] >= y, 1] += expansion_factor - 1
+
+    return new_coords
+
+
+def pairwise_distance(coords):
+    return np.sum(np.abs(coords[:, np.newaxis, :] - coords), axis=2)
+
+
+def cumulated_distances(universe, expansion_factor):
+    coords = get_galaxies_coords(universe, expansion_factor)
+    return np.sum(pairwise_distance(coords)) // 2
+
+
+# %%
+with open(puzzle(11), "r") as f:
     data = f.read().splitlines()
-    grid = np.array([[int(char == '#') for char in line] for line in data])
+    universe = np.array([[int(char == "#") for char in line] for line in data])
+
+print("Part 1 —", cumulated_distances(universe, 2))
+print("Part 2 —", cumulated_distances(universe, 1_000_000))
 
 # %%
-grid = np.insert(grid, np.where(np.sum(grid, axis=0) == 0)[0], 0, axis=1)
-grid = np.insert(grid, np.where(np.sum(grid, axis=1) == 0)[0], 0, axis=0)
-
-
-coords = np.argwhere(grid)
-distances = np.sum(np.abs(coords[:, np.newaxis, :] - coords), axis=2)
-
-print("Part 1 —", np.sum(distances) // 2)
-
-# %%
-
